90ab9e726b07a45f5e17c99ddf83d02ed63d0df0
Guilherme Sella <guilhermesella1974@gmail.com>
2023-09-30 15:10:23 -0300

    Criando função para validar informações customer

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import { ValidatorCustomer } from '../services/index.js';
 
 export const router = express.Router();
 
 
 router.post("/customer-loans", (req, res)=>{
 
+    
     const customerInfo = {
         age: req.body.age,
         cpf: req.body.cpf,
@@ -13,8 +15,8 @@ router.post("/customer-loans", (req, res)=>{
         location: req.body.location
     }
 
-    
+    const loans = ValidatorCustomer(customerInfo)
 
 
-    return res.status(200).json(customer)
+    return res.status(200).json(loans)
 })
---
+++ services/index.js
@@ -0,0 +1,49 @@
+
+
+export const ValidatorCustomer = (customerInfo) => {
+
+    const loans = [];
+
+    
+
+    if(customerInfo.income <= 3000){
+        loans.push(
+        {
+            type: "PERSONAL",
+            interest_rate: 4
+        },
+        {
+            type: "GUARANTEED",
+            interest_rate: 3
+        }
+     )
+    }
+    else if (customerInfo.income <= 5000 && customerInfo.income >= 3000 && customerInfo.age < 30 && customerInfo.location == "SP" ){
+        loans.push(
+            {
+                type: "PERSONAL",
+                interest_rate: 4
+            },
+            {
+                type: "GUARANTEED",
+                interest_rate: 3
+            }
+         )
+    }
+
+    // EMPRÉSTIMO CONSIGNADO
+
+    if(customerInfo.income >= 5000){
+        loans.push(
+            {
+                type: "CONSIGNMENT",
+                interest_rate: 2
+            }
+        )
+    }
+
+    
+
+    return loans;
+
+}
